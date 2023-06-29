67f2c5ac71c1c65c952bb25c4454a3dbcc8e5c70
Liming Cai <daybreak.chua@gmail.com>
2023-06-29 14:27:01 -0500

    Update genewiseLL_plot.R

--- Topology_tests/genewiseLL_plot.R
+++ Topology_tests/genewiseLL_plot.R
@@ -1,6 +1,25 @@
 library(ggplot2)
-df <- data.frame(order = 1:length(x$L.T1.T2.), numbers = x$L.T1.T2., color = ifelse(x$L.T1.T2. > 0, "red", "blue"))
+x=read.table('conTr5.AU.genewisell.wpl.partlh',header=T)
 
+x$LLsum[1]=x$deltaLL[1]
+for (i in (2:length(x$deltaLL))){
+x$LLsum[i]=x$LLsum[i-1]+x$deltaLL[i]
+}
+
+##accumulated likelihood
+df <- data.frame(order = 1:length(x$Tree1), numbers = x$LLsum, color = ifelse(x$LLsum > 0, "red", "blue"))
+pdf('conTr1.accumulatedLL.pdf',width=5,height=4)
+ggplot(df, aes(x = order, y = numbers, fill = color)) +
+	geom_bar(stat = "identity") +
+	scale_fill_identity() +
+	theme_bw() +
+	labs(x = "Gene", y = "accumulated LL1-LL2")
+
+dev.off()
+
+
+##likelihood per tree
+df <- data.frame(order = 1:length(x$Tree1), numbers = x$deltaLL, color = ifelse(x$deltaLL > 0, "red", "blue"))
 pdf('conTr1.LLcomparison.pdf',width=5,height=4)
 ggplot(df, aes(x = order, y = numbers, fill = color)) +
 	geom_bar(stat = "identity") +
