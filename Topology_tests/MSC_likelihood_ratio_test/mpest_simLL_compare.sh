44400c90adb986e81b872ccb4b7f2b3f56900ca8
Liming Cai <daybreak.chua@gmail.com>
2023-07-14 16:07:16 -0500

    Create mpest_simLL_compare.sh

---
+++ Topology_tests/MSC_likelihood_ratio_test/mpest_simLL_compare.sh
@@ -0,0 +1,300 @@
+echo sim000.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim001.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim002.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim003.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim004.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim005.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim006.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim007.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim008.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim009.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim010.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim011.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim012.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim013.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim014.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim015.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim016.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim017.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim018.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim019.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim020.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim021.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim022.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim023.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim024.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim025.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim026.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim027.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim028.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim029.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim030.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim031.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim032.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim033.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim034.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim035.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim036.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim037.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim038.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim039.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim040.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim041.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim042.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim043.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim044.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim045.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim046.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim047.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim048.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim049.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim050.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim051.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim052.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim053.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim054.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim055.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim056.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim057.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim058.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim059.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim060.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim061.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim062.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim063.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim064.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim065.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim066.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim067.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim068.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim069.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim070.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim071.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim072.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim073.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim074.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim075.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim076.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim077.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim078.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim079.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim080.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim081.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim082.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim083.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim084.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim085.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim086.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim087.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim088.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim089.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim090.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim091.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim092.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim093.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim094.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim095.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim096.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim097.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim098.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
+echo sim099.trees | cat - mpest.LL.ctl mpestML.tre >l
+mpest l | tail -1 >>ML_ML.LL
+
